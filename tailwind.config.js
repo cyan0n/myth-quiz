89908fd40893d767b90ac40838b4b62ff594b362
Cyan0n <cyan0n.dev@gmail.com>
2021-08-01 09:32:01 +0200

    Get styling to work

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        display: ['"Cinzel Decorative"', "cursive"],
+        main: ['"Nanum Gothic"', "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
