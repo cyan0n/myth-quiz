3ae55e4e45bf569f2ea113d93feef1a6cf01acc2
Cyan0n <cyan0n.dev@gmail.com>
2021-08-03 14:32:44 +0200

    "Working" example for session

---
+++ lib/session.ts
@@ -0,0 +1,25 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
+import { NextApiRequest, NextApiResponse } from "next";
+import { Session, withIronSession } from "next-iron-session";
+
+const { SESSION_PASS } = process.env;
+
+// optionally add stronger typing for next-specific implementation
+export type NextIronRequest = NextApiRequest & { session: Session };
+export type NextIronHandler = (
+  req: NextIronRequest,
+  res: NextApiResponse,
+) => void | Promise<void>;
+
+const withSession = (handler: NextIronHandler) =>
+  withIronSession(handler, {
+    password: SESSION_PASS as string,
+    cookieName: "next-iron-session/examples/next.js",
+    cookieOptions: {
+      // the next line allows to use the session in non-https environments like
+      // Next.js dev mode (http://localhost:3000)
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
+
+export default withSession;
