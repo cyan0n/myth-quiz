c756cb439bf40d58743236aa88d2f9c09382762f
Cyan0n <cyan0n.dev@gmail.com>
2021-08-06 01:18:30 +0200

    Basic style for results page

--- pages/[myth]/results.tsx
+++ pages/[myth]/results.tsx
@@ -1,10 +1,18 @@
+import { Space } from "antd";
 import React from "react";
+import PageLayout from "../../components/PageLayout";
 
 export interface MythResultsProps {}
 type MythResultsComponent = React.FC<MythResultsProps>;
 
 const MythResults: MythResultsComponent = ({}) => {
-  return <>MythResults</>;
+  return (
+    <PageLayout>
+      <Space direction="vertical">
+        <h1>Results</h1>
+      </Space>
+    </PageLayout>
+  );
 };
 
 export default MythResults;
