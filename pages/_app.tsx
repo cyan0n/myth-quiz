feba3b39f1c30ec5dbfb24f29c7b29187addfe1a
Cyan0n <cyan0n.dev@gmail.com>
2021-08-06 09:59:02 +0200

    Added a lot of shit to make antd custom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
+import "../less/antd-custom.less";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
